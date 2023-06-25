8d43b2f88331b8677c7d5d99e6fda8edec8e6143
unknown <pedro.silva@rarolabs.com.br>
2023-06-25 08:21:14 -0300

    feat: adicionando um router

--- src/main/index.tsx
+++ src/main/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM  from "react-dom";
-import { Login } from "@/presentation/pages/"
+import { Router } from "@/presentation/components";
+import '@/presentation/styles/global.scss'
 
 ReactDOM.render(
-    <Login/>, document.getElementById('main')
+    <Router />, document.getElementById('main')
 )
