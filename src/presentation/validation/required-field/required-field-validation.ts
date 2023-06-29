0853588224bb6beb4645b7e0ffa9a6020e12d6e7
unknown <pedro.silva@rarolabs.com.br>
2023-06-29 15:04:26 -0300

    test: deve retornar null caso campo esteja preenchido

---
+++ src/presentation/validation/required-field/required-field-validation.ts
@@ -0,0 +1,10 @@
+import { RequiredFieldError } from "@/presentation/validation/errors/";
+import { FieldValidation } from "@/presentation/validation/protocols/";
+
+export class RequiredFieldValidation implements FieldValidation {
+    constructor(readonly field: string) {}
+
+    validate(value: string): Error {
+        return value ? null : new RequiredFieldError()
+    }
+}
---
+++ src/presentation/validation/required-field/required-fild-validation.spec.tsx
@@ -0,0 +1,23 @@
+import { RequiredFieldError } from "@/presentation/validation/errors/"
+import { RequiredFieldValidation } from "./required-field-validation"
+import { faker } from '@faker-js/faker'
+const makeSut = () => {
+    return new RequiredFieldValidation('email')
+}
+
+describe('test campo obrigatorio', () => {
+    test('Deve retornar erro caso campo nao esteja preenchido', () => {
+        const sut = makeSut()
+
+        const error = sut.validate('')
+
+        expect(error).toEqual(new RequiredFieldError())
+    })
+    test('Deve retornar null caso o campo esteja preenchido', () => {
+        const sut = makeSut()
+
+        const error = sut.validate(faker.internet.email())
+
+        expect(error).toBeFalsy()
+    })
+})
