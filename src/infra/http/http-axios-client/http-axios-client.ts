46cbe35bf6d7c3be660b32c55b120673c85829ac
unknown <pedro.silva@rarolabs.com.br>
2023-06-30 13:56:33 -0300

    fix: fix response axios em caso de erro

--- src/infra/http/http-axios-client/http-axios-client.ts
+++ src/infra/http/http-axios-client/http-axios-client.ts
@@ -1,9 +1,14 @@
 import { HttpPostClient, HttpResponse, PostParams } from "@/data/protocols/http";
-import axios from "axios"
+import axios, { AxiosResponse } from "axios"
 
 export class AxiosHttpClient implements HttpPostClient<any, any> {
     async post(params: PostParams<any>): Promise<HttpResponse<any>> {
-        const result = await axios.post(params.url, params.body)
+        let result: AxiosResponse
+        try {
+            result = await axios.post(params.url, params.body)
+        }catch(Error) {
+            result = Error.response
+        }
         return {
             StatusCode: result.status,
             Body: result.data
--- src/infra/http/test/http/mock-axios.ts
+++ src/infra/http/test/http/mock-axios.ts
@@ -1,12 +1,14 @@
 import { faker } from "@faker-js/faker"
 import axios from "axios"
 
+export const mockResponse = (): any => ({
+    data: {prop1: faker.string.uuid()},
+    status: faker.number.int(3)
+})
+
 export const getMockedAxios = (): jest.Mocked<typeof axios> => {
     const mockedAxios = axios as jest.Mocked<typeof axios>
-    mockedAxios.post.mockResolvedValue({
-        data: {prop1: faker.string.uuid()},
-        status: faker.number.int(3)
-    })
+    mockedAxios.post.mockResolvedValue(mockResponse())
     return mockedAxios
 }
 
