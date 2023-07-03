040a803f38ec98367b87dcc92e19cbb3b0601e42
unknown <pedro.silva@rarolabs.com.br>
2023-07-03 17:26:17 -0300

    test: adicionando mais testes ao signup

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -14,22 +14,24 @@ type sutProps = {
 
 const makeSut = (props?: sutProps): sutTypes => {
   const validationSpy = new ValidationSpy()
-  validationSpy.errorMassage = props.validationError
+  validationSpy.errorMassage = props?.validationError
   const sut = render(<SignUp validation={validationSpy} />)
   return {
     sut
   }
 }
 
-describe('Login testes', () => {
+describe('SignUP testes', () => {
   test('Deve iniciar a tela com estado inicial', () => {
     const validationErro = 'Campo Obrigatório'
     const { sut } = makeSut({ validationError: validationErro })
     Helper.verificaNumeroDeFilhos(sut, 'form-status', 0)
     const button = sut.getByRole('button', { name: 'Cadastrar' }) as HTMLButtonElement
     expect(button.disabled).toBe(true)
+    Helper.verifcaFieldError(sut, 'name-status', validationErro)
     Helper.verifcaFieldError(sut, 'email-status', validationErro)
     Helper.verifcaFieldError(sut, 'password-status', validationErro)
+    Helper.verifcaFieldError(sut, 'confirmPassword-status', validationErro)
   })
   test('Deve setar valor do input status com o erro do validation name', () => {
     const validationErro = faker.word.words()
@@ -37,5 +39,61 @@ describe('Login testes', () => {
 
     Helper.preencheCampo(sut, 'name')
     Helper.verificaFieldStatus(sut, 'name-status', '❌', validationErro)
+  })
+  test('Deve setar valor do input status com o erro do validation email', () => {
+    const validationErro = faker.word.words()
+    const { sut } = makeSut({ validationError: validationErro })
+
+    Helper.preencheCampo(sut, 'email')
+    Helper.verificaFieldStatus(sut, 'email-status', '❌', validationErro)
+  })
+  test('Deve setar valor do input status com o erro do validation password', () => {
+    const validationErro = faker.word.words()
+    const { sut } = makeSut({ validationError: validationErro })
+
+    Helper.preencheCampo(sut, 'password')
+    Helper.verificaFieldStatus(sut, 'password-status', '❌', validationErro)
+  })
+  test('Deve setar valor do input status com o erro do validation confirmPassword', () => {
+    const validationErro = faker.word.words()
+    const { sut } = makeSut({ validationError: validationErro })
+
+    Helper.preencheCampo(sut, 'confirmPassword')
+    Helper.verificaFieldStatus(sut, 'confirmPassword-status', '❌', validationErro)
+  })
+  test('Deve setar valor do input status como vazio e mostrar sucesso name', () => {
+    const { sut } = makeSut()
+
+    Helper.preencheCampo(sut, 'name')
+    Helper.verificaFieldStatus(sut, 'name-status', '✅')
+  })
+  test('Deve setar valor do input status como vazio e mostrar sucesso email', () => {
+    const { sut } = makeSut()
+
+    Helper.preencheCampo(sut, 'email')
+    Helper.verificaFieldStatus(sut, 'email-status', '✅')
+  })
+  test('Deve setar valor do input status como vazio e mostrar sucesso password', () => {
+    const { sut } = makeSut()
+
+    Helper.preencheCampo(sut, 'password')
+    Helper.verificaFieldStatus(sut, 'password-status', '✅')
+  })
+  test('Deve setar valor do input status como vazio e mostrar sucesso confirmPassword', () => {
+    const { sut } = makeSut()
+
+    Helper.preencheCampo(sut, 'confirmPassword')
+    Helper.verificaFieldStatus(sut, 'confirmPassword-status', '✅')
+  })
+  test('Deve habilitar botao em caso de validate nao retornar erro', () => {
+    const { sut } = makeSut()
+
+    Helper.preencheCampo(sut, 'name')
+    Helper.preencheCampo(sut, 'email')
+    Helper.preencheCampo(sut, 'password')
+    Helper.preencheCampo(sut, 'confirmPassword')
+
+    const button = sut.getByRole('button', { name: 'Cadastrar' }) as HTMLButtonElement
+    expect(button.disabled).toBe(false)
   })
 })
--- src/presentation/pages/signup/signup.tsx
+++ src/presentation/pages/signup/signup.tsx
@@ -13,18 +13,24 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
   const [state, setState] = useState({
     isLoading: false,
     name: '',
-    nameError: 'Campo Obrigatório',
-    emailError: 'Campo Obrigatório',
-    passwordError: 'Campo Obrigatório',
-    confirmPasswordError: 'Campo Obrigatório'
+    email: '',
+    password: '',
+    confirmPassword: '',
+    nameError: '',
+    emailError: '',
+    passwordError: '',
+    confirmPasswordError: ''
   })
 
   useEffect(() => {
     setState({
       ...state,
-      nameError: validation.validate('name', state.name)
+      nameError: validation.validate('name', state.name),
+      emailError: validation.validate('email', state.email),
+      passwordError: validation.validate('password', state.password),
+      confirmPasswordError: validation.validate('confirmPassword', state.confirmPassword)
     })
-  }, [state.name])
+  }, [state.name, state.email, state.password, state.confirmPassword])
 
   return (
     <div className={Styles.login}>
@@ -36,7 +42,7 @@ const SignUp: React.FC<Props> = ({ validation }: Props) => {
           <Input type="email" name="email" id="email" placeholder="Digite seu email" />
           <Input type="password" name="password" id="password" placeholder="Digite sua senha" />
           <Input type="password" name="confirmPassword" id="confirmPassword" placeholder="Confirme sua senha" />
-          <button type="submit" disabled className={Styles.submit}>Cadastrar</button>
+          <button type="submit" disabled={!!state.nameError || !!state.emailError || !!state.passwordError || !!state.confirmPasswordError} className={Styles.submit}>Cadastrar</button>
           <span className={Styles.link}>Voltar para o login</span>
           <FormStatus />
         </form>
